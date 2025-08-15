d9de5f41e79d4567e498cde17d94ac17defadde2
Hisaw <hisawsarah321@gmail.com>
2025-08-15 02:24:03 -0700

    scroll to top

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,12 @@
 // src/components/Layout.jsx
 import Header from "./Header";
 import Footer from "./Footer";
+import ScrollToTop from "./ScrollToTop";
 
 const Layout = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen">
-      <Header />
+      <ScrollToTop /> <Header />
       <main className="flex-1">{children}</main>
       <Footer />
     </div>
